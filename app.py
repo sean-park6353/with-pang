6a380d790c29e9b0801ec7e09423afbeae8aeaf9
sean-park6353 <package9@naver.com>
2023-07-25 02:28:11 +0900

    로깅 추가

--- app.py
+++ app.py
@@ -7,6 +7,13 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from dotenv import load_dotenv
 from functools import wraps
 import os
+import logging
+
+# 로그 설정
+log_formatter = logging.Formatter('[%(asctime)s %(levelname)s] %(name)s - %(message)s')
+file_handler = logging.FileHandler('app.log')
+file_handler.setFormatter(log_formatter)
+file_handler.setLevel(logging.DEBUG)
 
 load_dotenv()
 app = Flask(__name__)
@@ -32,6 +39,10 @@ migrate = Migrate(app, db)
 with app.app_context():
     db.create_all()
 
+# 로그 설정 추가
+app.logger.addHandler(file_handler)
+app.logger.setLevel(logging.DEBUG)
+
 @app.route('/', methods=['GET', 'POST'])
 def main():
     if request.method == 'GET':
@@ -40,6 +51,7 @@ def main():
 
 @app.route('/signin', methods=['POST'])
 def signin():
+    app.logger.info("signin 요청")
     data = request.get_json()
     login_id = data.get('signinId')
     password = data.get('signinPw')
@@ -47,14 +59,16 @@ def signin():
 
     if user and check_password_hash(user.password, password):
         session["user_id"] = user.id
+        app.logger.info(f"signin 성공: {login_id}")
         response = {"result": "성공", "code": "S001"}
         return make_response(jsonify(response), 200)
-
+    app.logger.warning("signin 실패: 아이디와 패스워드를 확인해주세요")
     response = {"result": "아이디와 패스워드를 확인해주세요", "code": "E001"}
     return make_response(jsonify(response), 400)
 
 @app.route('/signup', methods=['POST'])
 def signup():
+    app.logger.info("signup 요청")
     data = request.get_json()
     login_id = data.get('signupId')
     password = data.get('signupPw')
@@ -62,6 +76,7 @@ def signup():
     # 이미 존재하는 사용자인지 확인
     user = User.query.filter_by(login_id=login_id).first()
     if user:
+        app.logger.warning(f"signup 실패: 이미 가입된 회원입니다. user_id: {user.id}")
         response = {"result": "이미 가입된 회원입니다", "code": "E001"}
         return make_response(jsonify(response), 400)
 
@@ -70,6 +85,7 @@ def signup():
     db.session.add(new_user)
     db.session.commit()
 
+    app.logger.info(f"signup 성공. login_id: {login_id}, ") 
     response = {"result": "성공", "code": "S001"}
     return make_response(jsonify(response), 200)
 
@@ -81,14 +97,17 @@ def dashboard():
     user_id = session.get('user_id')
     if user_id:
         user = User.query.filter_by(id=user_id).first()
+        app.logger.info(f'Welcome, {user.login_id}! This is your dashboard.')
         return f'Welcome, {user.login_id}! This is your dashboard.'
     else:
+        app.logger.error("사용자 세션 정보가 없음")  # 로그 추가
         return redirect(url_for('login'))
     
 # 로그아웃 라우터
 @app.route('/logout')
 @required_login
 def logout():
+    app.logger.info("사용자 로그아웃 login_id: {session.get(user_id)}")  # 로그 추가
     session.clear()
     return redirect(url_for('login'))
 

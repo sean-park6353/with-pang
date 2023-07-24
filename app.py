bdfc3093796d9a35d9f7cf758e809481880747bf
sean-park6353 <package9@naver.com>
2023-07-24 15:12:52 +0900

    여덟번째 커밋

--- app.py
+++ app.py
@@ -85,4 +85,4 @@ def logout():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=443, ssl_context='adhoc')
+    app.run(host='0.0.0.0', port=443, ssl_context=('cert.pem', 'key.pem'))
